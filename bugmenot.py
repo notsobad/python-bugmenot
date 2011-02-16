2903803717115a78aaf555aae33174e59a51b0ae
Antonio Ospite <ospite@studenti.unina.it>
2011-02-16 10:03:07 +0100

    Fixed other pep8 style issues
    Style checked with:
      pep8 --ignore=E501 bugmenot.py
    
    Fixed issues:
    bugmenot.py:9:20: W291 trailing whitespace
    bugmenot.py:16:15: E401 multiple imports on one line
    bugmenot.py:17:10: E231 missing whitespace after ','
    bugmenot.py:19:1: E302 expected 2 blank lines, found 1
    bugmenot.py:27:44: E202 whitespace before '}'
    bugmenot.py:33:119: E203 whitespace before ','
    bugmenot.py:35:55: E261 at least two spaces before inline comment
    bugmenot.py:40:141: E225 missing whitespace around operator
    bugmenot.py:42:15: E222 multiple spaces after operator
    bugmenot.py:52:45: E251 no spaces around keyword / parameter equals
    bugmenot.py:77:1: W391 blank line at end of file
    
    Signed-off-by: Antonio Ospite <ospite@studenti.unina.it>
    Signed-off-by: notsobad <wangxiaohugg@gmail.com>

--- bugmenot.py
+++ bugmenot.py
@@ -6,40 +6,45 @@
 Get username and password from http://www.bugmenot.com/
 
 File: bugmynot.py
-Author: notsobad.me 
+Author: notsobad.me
 Description:
 Created: 2009-11-09 15:23:41
 Last modified: 2010.11.25
-
 '''
+import urllib2
+import re
+import pprint
 
-import urllib2, re, pprint
-import os,sys,traceback,optparse
+import os
+import sys
+import traceback
+import optparse
+
 
 class BugMeNot:
     def __init__(self):
         pass
 
     def _get_account(self, host):
-        headers = {
-                'User-Agent': 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)',
-                'Pragma': 'no-cache',
-                'Cache-Control': 'no-cache'      
-                }
+        headers = dict()
+        headers['User-Agent'] = 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)'
+        headers['Pragma'] = 'no-cache'
+        headers['Cache-Control'] = 'no-cache'
 
         try:
             urlOpener = urllib2.build_opener()
 
-            request = urllib2.Request('http://www.bugmenot.com/view/%s?utm_source=extension&utm_medium=firefox' % host , None, headers)
+            request = urllib2.Request('http://www.bugmenot.com/view/%s?utm_source=extension&utm_medium=firefox' % host, None, headers)
 
-            page = urlOpener.open(request).read(50000) # Log in BugMeNot
+            page = urlOpener.open(request).read(50000)  # Log in BugMeNot
         except (urllib2.HTTPError, urllib2.URLError):
             print >> sys.stderr, 'Http Error! Please check the url you input and the network connection'
             sys.exit(2)
 
-        re_loginpwd = re.compile(u'<tr><th>Username </th><td>(.+?)</td></tr>[^<]+?<tr><th>Password </th><td>([^<]*)</td></tr>',re.IGNORECASE|re.DOTALL)
+        re_loginpwd = re.compile(u'<tr><th>Username </th><td>(.+?)</td></tr>[^<]+?<tr><th>Password </th><td>([^<]*)</td></tr>',
+                re.IGNORECASE | re.DOTALL)
 
-        match =  re_loginpwd.findall(page)
+        match = re_loginpwd.findall(page)
         #return [(i, j) for i, j in match if i and j and len(i) < 30]
         return [{'username':i, 'password':j} for i, j in match if i and j and len(i) < 30]
 
@@ -49,8 +54,8 @@ class BugMeNot:
 
 if __name__ == '__main__':
     parser = optparse.OptionParser()
-    parser.add_option("-s", "--site", dest = "site",help = "The target site")
-    parser.add_option("-t", "--ret_type", dest="ret",default="text", help="The return type(text/json)")
+    parser.add_option("-s", "--site", dest="site", help="The target site")
+    parser.add_option("-t", "--ret_type", dest="ret", default="text", help="The return type(text/json)")
 
     (options, args) = parser.parse_args()
 
@@ -74,4 +79,3 @@ if __name__ == '__main__':
         sys.exit(1)
 
     sys.exit(0)
-
