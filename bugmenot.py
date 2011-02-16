86339a633db33640c9585c616168d0698a22fb24
Antonio Ospite <ospite@studenti.unina.it>
2011-02-16 11:30:52 +0100

    Print extended info in the text output
    Print the 'Other' and 'Stats' info from bugmenot, but leave that
    optional for the text output. In the json output every field is exported
    unconditionally.
    
    Signed-off-by: Antonio Ospite <ospite@studenti.unina.it>
    Signed-off-by: notsobad <wangxiaohugg@gmail.com>

--- bugmenot.py
+++ bugmenot.py
@@ -20,7 +20,10 @@ import re
 
 class BugMeNot:
     def __init__(self):
-        pass
+        self.regex = u'<tr><th>Username </th><td>([^<]*)</td></tr>'
+        self.regex += u'[^<]+?<tr><th>Password </th><td>([^<]*)</td></tr>'
+        self.regex += u'[^<]+?<tr><th>Other</th><td>([^<]*)</td></tr>'
+        self.regex += u'[^<]+?<tr><th>Stats</th><td class="stats"><em class="[^"]*">([0-9]*%)[^<]*</em>[^<]*</td></tr>'
 
     def _get_account(self, host):
         headers = dict()
@@ -38,12 +41,11 @@ class BugMeNot:
             print >> sys.stderr, 'Http Error! Please check the url you input and the network connection'
             sys.exit(2)
 
-        re_loginpwd = re.compile(u'<tr><th>Username </th><td>(.+?)</td></tr>[^<]+?<tr><th>Password </th><td>([^<]*)</td></tr>',
-                re.IGNORECASE | re.DOTALL)
+        re_loginpwd = re.compile(self.regex, re.IGNORECASE | re.DOTALL)
 
         match = re_loginpwd.findall(page)
         #return [(i, j) for i, j in match if i and j and len(i) < 30]
-        return [{'username':i, 'password':j} for i, j in match if i and j and len(i) < 30]
+        return [{'username':i, 'password':j, 'other':o, 'stats':s} for i, j, o, s in match if i and j and len(i) < 30]
 
     def get_account(self, host):
         return self._get_account(host)
@@ -51,6 +53,9 @@ class BugMeNot:
 
 if __name__ == '__main__':
     parser = optparse.OptionParser()
+
+    parser.add_option("-e", "--extended_info", dest="extended_info",
+            action="store_true", default=False, help="Show extended info in the text mode")
     parser.add_option("-s", "--site", dest="site", help="The target site")
     parser.add_option("-t", "--ret_type", dest="ret", default="text", help="The return type(text/json)")
 
@@ -64,10 +69,18 @@ if __name__ == '__main__':
             sys.exit(1)
 
         if options.ret == 'text':
-            print "%-30s\t%-20s" % ("Username", "Password")
-            print "-" * 50
+            print "%-30s\t%-20s" % ("Username", "Password"),
+            line_len = 30 + 20
+            if options.extended_info:
+                print "\t%-25s\t%-5s" % ("Other", "Stats"),
+                line_len += 25 + 20
+
+            print "\n", "-" * line_len
             for account in accounts:
-                print "%(username)-30s\t%(password)-20s" % account
+                print "%(username)-30s\t%(password)-20s" % account,
+                if options.extended_info:
+                    print "\t%(other)-25s\t%(stats)-5s" % account,
+                print
         elif options.ret == 'json':
             import json
             print json.dumps(accounts)
